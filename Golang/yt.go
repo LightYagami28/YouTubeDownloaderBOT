101d40acad57ebe4e583ae09733ee383aac1b073
Light Yagami <Maule2703@gmail.com>
2024-07-24 19:19:24 +0200

    Update file golang

--- Golang/yt.go
+++ Golang/yt.go
@@ -3,13 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 type config struct {
@@ -69,37 +69,37 @@ func main() {
 	}
 
 	// Start command
-	bot.Handle("/start", func(update *tgbotapi.Update) {
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "*Hello!* __Send me a YouTube link!__")
-		msg.ParseMode = "markdown"
-		bot.Send(msg)
-	})
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-	// Handle YouTube link
-	bot.Handle(tgbotapi.UpdateTypeMessage, func(update *tgbotapi.Update) {
-		text := update.Message.Text
-		if strings.Contains(text, "https://www.youtube.com/") || strings.Contains(text, "https://youtu.be/") {
-			link = text
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.Text == "/start" {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "*Hello!* __Send me a YouTube link!__")
+			msg.ParseMode = "markdown"
+			bot.Send(msg)
+		} else if strings.Contains(update.Message.Text, "https://www.youtube.com/") || strings.Contains(update.Message.Text, "https://youtu.be/") {
+			link = update.Message.Text
 			resolutions := []string{"720p", "480p", "360p", "240p", "144p"}
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Pick a resolution:")
 			msg.ReplyMarkup = createResolutionButtons(resolutions)
 			bot.Send(msg)
+		} else if update.CallbackQuery != nil {
+			resolution := update.CallbackQuery.Data
+			chatID := update.CallbackQuery.Message.Chat.ID
+			bot.AnswerCallbackQuery(tgbotapi.NewCallback(update.CallbackQuery.ID, ""))
+			bot.DeleteMessage(tgbotapi.DeleteMessageConfig{
+				ChatID:    chatID,
+				MessageID: update.CallbackQuery.Message.MessageID,
+			})
+			msg := tgbotapi.NewMessage(chatID, "Downloading...")
+			bot.Send(msg)
+			downloadAndSendVideo(chatID, link, resolution)
 		}
-	})
-
-	// Handle resolution selection
-	bot.Handle(tgbotapi.UpdateTypeCallbackQuery, func(update *tgbotapi.Update) {
-		resolution := update.CallbackQuery.Data
-		chatID := update.CallbackQuery.Message.Chat.ID
-		bot.AnswerCallbackQuery(tgbotapi.NewCallback(update.CallbackQuery.ID, ""))
-		bot.DeleteMessage(tgbotapi.DeleteMessageConfig{
-			ChatID:    chatID,
-			MessageID: update.CallbackQuery.Message.MessageID,
-		})
-		msg := tgbotapi.NewMessage(chatID, "Downloading...")
-		bot.Send(msg)
-		downloadAndSendVideo(chatID, link, resolution)
-	})
-
-	bot.Receive()
+	}
 }
